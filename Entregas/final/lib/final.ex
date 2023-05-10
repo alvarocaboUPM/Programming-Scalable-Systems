4bc3ea8f0e922cad2d91c2088e4dc761fc5f4379
alvarocabo <alvarocaboac2@gmail.com>
2023-05-10 18:34:51 +0200

    Project module added

--- Entregas/final/lib/final.ex
+++ Entregas/final/lib/final.ex
@@ -2,6 +2,7 @@ defmodule Final do
   @moduledoc """
   Entrega final evaluable
   """
+
   def date_parts(date_str) do
     String.split(date_str, "-")
     |> Enum.map(&String.to_integer/1)
@@ -22,17 +23,181 @@ defmodule Final do
   end
 
   @spec map([a], (a -> b)) :: [b] when a: any(), b: any()
-  def map(list, func) do
-    for item <- list, do: func.(item)
+  def map([], _func), do: []
+
+  def map([h|t], func) do
+    [func.(h) | map(t, func)]
   end
 
   @spec reduce(list :: [a], acc :: b, func :: (a, b -> b)) :: b when a: any(), b: any()
-  def reduce(list, acc, func) do
-    Enum.reduce(list, acc, func)
+  def reduce([], acc, _func), do: acc
+
+  def reduce([h|t], acc, func) do
+    acc= func.(h,acc)
+    reduce(t, acc, func)
   end
 
   @spec filter(list :: [a], func :: (a -> boolean())) :: [a] when a: any()
-  def filter(list, func) do
-    Enum.filter(list, func)
+  def filter([], _func), do: []
+
+  def filter([h|t], func) do
+    res = func.(h)
+    if res == true do
+      [h | filter(t, func)]
+    else
+      filter(t, func)
+    end
+  end
+
+  defmodule Bank do
+    #  Creates a bank and returns a "bank" pid
+    def create_bank() do
+      spawn(fn -> loop(%{accounts: %{}, next_account_id: 1}) end)
+    end
+
+    defp loop(state) do
+      receive do
+        {:new_account, from, account} ->
+          new_state = new_account(pid_to_state(state), account)
+          send(from, {:ok, state_to_pid(new_state)})
+          loop(state)
+
+        {:withdraw, from, account_id, amount} ->
+          new_state = withdraw(pid_to_state(state), account_id, amount)
+          send(from, {:ok, state_to_pid(new_state)})
+          loop(state)
+
+        {:deposit, from, account_id, amount} ->
+          new_state = deposit(pid_to_state(state), account_id, amount)
+          send(from, {:ok, state_to_pid(new_state)})
+          loop(state)
+
+        {:transfer, from, account_id_from, account_id_to, amount} ->
+          new_state = transfer(pid_to_state(state), account_id_from, account_id_to, amount)
+          send(from, {:ok, state_to_pid(new_state)})
+          loop(state)
+
+        {:balance, from, account_id} ->
+          send(from, {:ok, balance(pid_to_state(state), account_id)})
+          loop(state)
+      end
+    end
+
+    defp pid_to_state(pid) do
+      :sys.get_state(pid)
+    end
+
+    defp state_to_pid(state) do
+      # Use :erlang.term_to_binary to convert the state to a binary,
+      # and then spawn a new process that loads the binary into its state.
+      binary_state = state_to_binary(state)
+
+      spawn(fn ->
+        :sys.replace_state(self(), :erlang.binary_to_term(binary_state))
+        loop(:sys.get_state(self()))
+      end)
+    end
+
+    defp state_to_binary(state) do
+      # Use :erlang.term_to_binary to convert the state to a binary.
+      :erlang.term_to_binary(state)
+    end
+
+    # Creates a new account with account balance 0. Returns true if the
+    # account could be created (it was new) and false otherwise.
+    def new_account(bank_pid, account) do
+      case :sys.get_state(bank_pid) do
+        %{accounts: accounts} ->
+          case Map.get(accounts, account) do
+            nil ->
+              new_accounts = Map.put(accounts, account, 0)
+              :sys.replace_state(bank_pid, %{accounts: new_accounts})
+              {:ok, true}
+
+            _ ->
+              {:ok, false}
+          end
+
+        _ ->
+          {:error, "Bank not found"}
+      end
+    end
+
+    # Withdraws money from the account (if quantity =< account balance).
+    # Returns the amount of money withdrawn.
+    def withdraw(bank_pid, account, quantity) do
+      case :sys.get_state(bank_pid) do
+        %{accounts: accounts} ->
+          case Map.get(accounts, account) do
+            nil ->
+              {:error, "Account not found"}
+
+            balance ->
+              if balance >= quantity do
+                new_balance = balance - quantity
+                new_accounts = Map.put(accounts, account, new_balance)
+                :sys.replace_state(bank_pid, %{accounts: new_accounts})
+                {:ok, new_balance}
+              else
+                {:error, "Insufficient funds"}
+              end
+          end
+
+        _ ->
+          {:error, "Invalid bank PID"}
+      end
+    end
+
+    # Increases balance of account by quantity, returning the new balance.
+    def deposit(bank_pid, account, quantity) do
+      case :sys.get_state(bank_pid) do
+        %{accounts: accounts} ->
+          case Map.get(accounts, account) do
+            nil ->
+              {:error, "Account not found"}
+
+            balance ->
+              new_balance = balance + quantity
+              new_accounts = Map.put(accounts, account, new_balance)
+              :sys.replace_state(bank_pid, %{accounts: new_accounts})
+              {:ok, new_balance}
+          end
+
+        _ ->
+          {:error, "Invalid bank PID"}
+      end
+    end
+
+    # Transfers quantity from one account (if the balance is sufficient)
+    # to another account. Returns the amount of money transferred.
+    def transfer(bank_pid, from_account, to_account, quantity) do
+      case withdraw(bank_pid, from_account, quantity) do
+        {:ok, amount} ->
+          case deposit(bank_pid, to_account, amount) do
+            {:ok, new_balance} ->
+              {:ok, amount}
+
+            error ->
+              error
+          end
+
+        error ->
+          error
+      end
+    end
+
+    # Returns the current balance for the account
+    def balance(bank_pid, account) do
+      case :sys.get_state(bank_pid) do
+        %{accounts: accounts} ->
+          case Map.get(accounts, account) do
+            nil -> {:error, "Account not found"}
+            balance -> {:ok, balance}
+          end
+
+        _ ->
+          {:error, "Invalid bank PID"}
+      end
+    end
   end
 end
--- Entregas/final/test/final_test.exs
+++ Entregas/final/test/final_test.exs
@@ -2,7 +2,84 @@ defmodule FinalTest do
   use ExUnit.Case
   doctest Final
 
-  test "greets the world" do
-    assert Final.hello() == :world
+  describe "create_bank/0" do
+    test "returns a pid" do
+      bank = Final.Bank.create_bank()
+      assert is_pid(bank)
+    end
+  end
+
+  describe "new_account/2" do
+    test "creates a new account with zero balance" do
+      bank = Final.Bank.create_bank()
+      assert Final.Bank.new_account(bank, "account1")
+      assert Final.Bank.new_account(bank, "account2")
+      assert Final.Bank.balance(bank, "account1") == 0
+      assert Final.Bank.balance(bank, "account2") == 0
+    end
+
+    test "returns false if the account already exists" do
+      bank = Final.Bank.create_bank()
+      assert Final.Bank.new_account(bank, "account1")
+      assert !Final.Bank.new_account(bank, "account1")
+    end
+  end
+
+  describe "deposit/3" do
+    test "increases account balance" do
+      bank = Final.Bank.create_bank()
+      Final.Bank.new_account(bank, "account1")
+      assert Final.Bank.deposit(bank, "account1", 100) == 100
+      assert Final.Bank.balance(bank, "account1") == 100
+    end
+  end
+
+  describe "withdraw/3" do
+    test "decreases account balance if sufficient balance is available" do
+      bank = Final.Bank.create_bank()
+      Final.Bank.new_account(bank, "account1")
+      Final.Bank.deposit(bank, "account1", 100)
+      assert Final.Bank.withdraw(bank, "account1", 50) == 50
+      assert Final.Bank.balance(bank, "account1") == 50
+    end
+
+    test "returns 0 and does not change balance if insufficient balance is available" do
+      bank = Final.Bank.create_bank()
+      Final.Bank.new_account(bank, "account1")
+      Final.Bank.deposit(bank, "account1", 100)
+      assert Final.Bank.withdraw(bank, "account1", 150) == 0
+      assert Final.Bank.balance(bank, "account1") == 100
+    end
+  end
+
+  describe "transfer/4" do
+    test "transfers money from one account to another" do
+      bank = Final.Bank.create_bank()
+      Final.Bank.new_account(bank, "account1")
+      Final.Bank.new_account(bank, "account2")
+      Final.Bank.deposit(bank, "account1", 100)
+      assert Final.Bank.transfer(bank, "account1", "account2", 50) == 50
+      assert Final.Bank.balance(bank, "account1") == 50
+      assert Final.Bank.balance(bank, "account2") == 50
+    end
+
+    test "returns 0 and does not change balance if insufficient balance is available" do
+      bank = Final.Bank.create_bank()
+      Final.Bank.new_account(bank, "account1")
+      Final.Bank.new_account(bank, "account2")
+      Final.Bank.deposit(bank, "account1", 100)
+      assert Final.Bank.transfer(bank, "account1", "account2", 150) == 0
+      assert Final.Bank.balance(bank, "account1") == 100
+      assert Final.Bank.balance(bank, "account2") == 0
+    end
+  end
+
+  describe "balance/2" do
+    test "returns the current balance for the account" do
+      bank = Final.Bank.create_bank()
+      Final.Bank.new_account(bank, "account1")
+      Final.Bank.deposit(bank, "account1", 100)
+      assert Final.Bank.balance(bank, "account1") == 100
+    end
   end
 end
